d1a83b674f81d51f2ddbbc75a6e1bd929ecc482e
Sarah Widder <sarah.j.widder@gmail.com>
2023-04-07 10:12:47 -0700

    Add sample app code

---
+++ app.py
@@ -0,0 +1,77 @@
+import os
+import logging
+import requests
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", defaults={"path": "index.html"})
+@app.route("/<path:path>")
+def static_file(path):
+    return app.send_static_file(path)
+
+# ACS Integration Settings
+AZURE_SEARCH_SERVICE = os.environ.get("AZURE_SEARCH_SERVICE")
+AZURE_SEARCH_INDEX = os.environ.get("AZURE_SEARCH_INDEX")
+AZURE_SEARCH_KEY = os.environ.get("AZURE_SEARCH_KEY")
+AZURE_SEARCH_USE_SEMANTIC_SEARCH = os.environ.get("AZURE_SEARCH_USE_SEMANTIC_SEARCH")
+AZURE_SEARCH_INDEX_IS_PRECHUNKED = os.environ.get("AZURE_SEARCH_INDEX_IS_PRECHUNKED")
+
+# AOAI Integration Settings
+AZURE_OPENAI_RESOURCE = os.environ.get("AZURE_OPENAI_RESOURCE")
+AZURE_OPENAI_MODEL = os.environ.get("AZURE_OPENAI_MODEL")
+AZURE_OPENAI_KEY = os.environ.get("AZURE_OPENAI_KEY")
+
+# Private API Management Key (temporary)
+AZURE_APIM_KEY = os.environ.get("AZURE_APIM_KEY")
+
+@app.route("/conversation", methods=["POST"])
+def conversation():
+    try:
+        messages = request.json["messages"]
+        body = {
+            "messages": messages
+        }
+        
+        azure_openai_url = f"https://{AZURE_OPENAI_RESOURCE}.openai.azure.com/openai/deployments/{AZURE_OPENAI_MODEL}/completions?api-version=2022-12-01"
+        search_url = f"https://{AZURE_SEARCH_SERVICE}.search.windows.net"
+
+        headers = {
+            "Content-Type": "application/json",
+            "azure_document_search_url": search_url,
+            "azure_document_search_api_key": AZURE_SEARCH_KEY,
+            "azure_document_search_index": AZURE_SEARCH_INDEX,
+            "azure_document_is_prechunked": AZURE_SEARCH_INDEX_IS_PRECHUNKED,
+            "chatgpt_url": azure_openai_url,
+            "chatgpt_key": AZURE_OPENAI_KEY,
+            "Ocp-Apim-Subscription-Key": AZURE_APIM_KEY,
+            "azure_document_search_configuration": "default" if AZURE_SEARCH_USE_SEMANTIC_SEARCH else "",
+            "azure_document_search_query_type": "semantic" if AZURE_SEARCH_USE_SEMANTIC_SEARCH else "simple"
+        }
+
+        endpoint = "https://tadevusw2.azure-api.net/svc/inferenceservice/conversation"
+        r = requests.post(endpoint, headers=headers, json=body)
+        r = r.json()
+
+        return jsonify(r)
+    except Exception as e:
+        logging.exception("Exception in /conversation")
+        return jsonify({"error": str(e)}), 500
+
+@app.route("/feedback", methods=["POST"])
+def feedback():
+    try:
+        headers = {
+            "Content-Type": "application/json",
+            "Ocp-Apim-Subscription-Key": AZURE_APIM_KEY
+        }
+        endpoint = "https://tadevusw2.azure-api.net/svc/inferenceservice/feedback"
+        r = requests.post(endpoint, headers=headers, json=request.json)
+        return jsonify({"status": r.status_code, "ok": r.ok})
+    except Exception as e:
+        logging.exception("Exception in /feedback")
+        return jsonify({"error": str(e)}), 500
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ frontend/src/pages/layout/Layout.tsx
@@ -0,0 +1,37 @@
+import { Outlet, NavLink, Link } from "react-router-dom";
+
+import github from "../../assets/github.svg";
+
+import styles from "./Layout.module.css";
+
+const Layout = () => {
+    return (
+        <div className={styles.layout}>
+            <header className={styles.header} role={"banner"}>
+                <div className={styles.headerContainer}>
+                    <Link to="/" className={styles.headerTitleContainer}>
+                        <h3 className={styles.headerTitle}>Azure OpenAI</h3>
+                    </Link>
+                    <nav>
+                        <ul className={styles.headerNavList}>
+                            <li>
+                                <NavLink to="/" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
+                                    Chat
+                                </NavLink>
+                            </li>
+                            <li className={styles.headerNavLeftMargin}>
+                                <NavLink to="/qa" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
+                                    Ask a question
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+
+            <Outlet />
+        </div>
+    );
+};
+
+export default Layout;
