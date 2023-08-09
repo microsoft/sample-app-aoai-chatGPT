821819ed80df0b6c57c01a16cba19481cee844ca
Rimvydas <rimvel@users.noreply.github.com>
2023-08-09 15:32:45 +0300

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -235,7 +235,7 @@ const Chat = () => {
                                     onKeyDown={e => e.key === "Enter" || e.key === " " ? stopGenerating() : null}
                                     >
                                         <SquareRegular className={styles.stopGeneratingIcon} aria-hidden="true"/>
-                                        <span className={styles.stopGeneratingText} aria-hidden="true">Stop generating</span>
+                                        <span className={styles.stopGeneratingText} aria-hidden="true">Stop generating...</span>
                                 </Stack>
                             )}
                             <div
