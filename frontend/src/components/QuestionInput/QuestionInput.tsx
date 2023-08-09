037a18930a33fb43560b79dd00e4002be1cfb252
derbuihan <derbuihan@gmail.com>
2023-08-10 01:45:45 +0900

    ignore Enter on composition

--- frontend/src/components/QuestionInput/QuestionInput.tsx
+++ frontend/src/components/QuestionInput/QuestionInput.tsx
@@ -27,7 +27,7 @@ export const QuestionInput = ({ onSend, disabled, placeholder, clearOnSend }: Pr
     };
 
     const onEnterPress = (ev: React.KeyboardEvent<Element>) => {
-        if (ev.key === "Enter" && !ev.shiftKey) {
+        if (ev.key === "Enter" && !ev.shiftKey && !(ev.nativeEvent?.isComposing === true)) {
             ev.preventDefault();
             sendQuestion();
         }
