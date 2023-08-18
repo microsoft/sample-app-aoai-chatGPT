57e34161d2d6073ba22ee9e66d933a7848520323
Pranjal Patel <pranjal.patel@bitscape.com>
2023-08-18 12:55:10 -0400

    changed body title

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -173,8 +173,8 @@ const Chat = () => {
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>Start chatting</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your questions</h2>
+                                <h1 className={styles.chatEmptyStateTitle}>Hi there! ask me anything</h1>
+                                <h2 className={styles.chatEmptyStateSubtitle}>Bitscape Velocity-AI is designed to answer your questions</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px"}} role="log">
