030e95cbe3a65b0327ea7cef09adbf646ca66ff1
Eirik Berge <58091970+ebbeberge@users.noreply.github.com>
2023-08-30 17:17:28 +0200

    Update Layout.tsx

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -84,7 +84,7 @@ const Layout = () => {
                     </Stack>
                     <Stack horizontal tokens={{ childrenGap: 4 }}>
                             {(appStateContext?.state.isCosmosDBAvailable?.status !== CosmosDBStatus.NotConfigured) && 
-                                <HistoryButton onClick={handleHistoryClick} text={appStateContext?.state?.isChatHistoryOpen ? "Hide chat history" : "Show chat history"}/>    
+                                <HistoryButton onClick={handleHistoryClick} text={appStateContext?.state?.isChatHistoryOpen ? "Lukk historikken" : "Åpne historikken"}/>    
                             }
                             <ShareButton onClick={handleShareClick} />
                     </Stack>
