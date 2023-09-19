0a719794a7609eddb5bbc2ba6d322fec7a16b76e
Maurice Ollenburg <ollenbum@apetito.com>
2023-09-19 16:03:17 +0200

    adjust colors

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,25 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter, Routes, Route } from "react-router-dom";
-import { initializeIcons } from "@fluentui/react";
+import {HashRouter, Routes, Route} from "react-router-dom";
+import {initializeIcons, loadTheme} from "@fluentui/react";
 
 import "./index.css";
 
 import Layout from "./pages/layout/Layout";
 import NoPage from "./pages/NoPage";
 import Chat from "./pages/chat/Chat";
-import { AppStateProvider } from "./state/AppProvider";
+import {AppStateProvider} from "./state/AppProvider";
 
 initializeIcons();
+
+loadTheme({
+    palette: {
+        themePrimary: "#37772F",
+        themeSecondary: "#37772F",
+        themeTertiary: "#37772F",
+    }
+})
 
 export default function App() {
     return (
         <AppStateProvider>
             <HashRouter>
                 <Routes>
-                    <Route path="/" element={<Layout />}>
-                        <Route index element={<Chat />} />
-                        <Route path="*" element={<NoPage />} />
+                    <Route path="/" element={<Layout/>}>
+                        <Route index element={<Chat/>}/>
+                        <Route path="*" element={<NoPage/>}/>
                     </Route>
                 </Routes>
             </HashRouter>
@@ -29,6 +37,6 @@ export default function App() {
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <App/>
     </React.StrictMode>
 );
