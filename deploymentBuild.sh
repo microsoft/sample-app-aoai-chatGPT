f8041c13f1eedc3942a0fe780f36fd023f1e190e
David Moses <davidmoses@microsoft.com>
2023-09-22 10:38:40 -0700

    updated script to get latest npm and node versions

--- deploymentBuild.sh
+++ deploymentBuild.sh
@@ -1,14 +1,47 @@
 #!/bin/bash
 
-# Update package list and install npm
+# Remove existing versions of Node.js and npm
 echo ""
-echo "Running apt-get update and install npm"
+echo "Removing existing versions of Node.js and npm"
 echo ""
-apt-get update && apt-get install -y npm
+apt-get remove -y nodejs npm
+
+# Install nvm and update Node.js to the latest version
+echo ""
+echo "Installing nvm and updating Node.js"
+echo ""
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+nvm install node
 if [ $? -ne 0 ]; then
-    echo "Failed to install npm"
+    echo "Failed to update Node.js"
     exit $?
 fi
+
+# # Install npm
+# echo ""
+# echo "Installing npm"
+# echo ""
+# apt-get install -y --no-install-recommends npm
+# if [ $? -ne 0 ]; then
+#     echo "Failed to install npm"
+#     exit $?
+# fi
+
+# Update package list and install npm
+echo ""
+echo "Running apt-get update and upgrade and install npm"
+echo ""
+apt-get update -y && apt-get upgrade -y && apt-get install -y --no-install-recommends npm
+if [ $? -ne 0 ]; then
+    echo "Failed to upgrade packages and install npm"
+    exit $?
+fi
+
+echo ""
+echo "---------------- TESTINGCASE ----------------"
+echo "Node.js version: $(node -v)"
+echo "npm version: $(npm -v)"
+echo ""
 
 # Restore and build frontend using npm
 echo ""
