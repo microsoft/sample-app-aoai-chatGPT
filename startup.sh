30e68274735bdab29a276c87575841c7c00bb7c9
David Moses <davidmoses@microsoft.com>
2023-09-27 11:25:56 -0700

    startup file

---
+++ startup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo ""
+echo "Installing requirements"
+echo ""
+cd streamlit
+pip install -r requirements.txt
+if [ $? -ne 0 ]; then
+    echo "Failed to install required packages"
+    exit $?
+fi
+
+echo ""
+echo "Launching streamlit"
+echo ""
+streamlit run chat.py --server.port 8080
+if [ $? -ne 0 ]; then
+    echo "Failed to launch streamlit"
+    exit $?
+fi
