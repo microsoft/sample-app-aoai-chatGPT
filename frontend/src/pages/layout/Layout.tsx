69f9775f057936159471678a4f64eb286ee736c6
mgueiros0white <148125315+mgueiros0white@users.noreply.github.com>
2023-10-17 11:48:38 -0300

    Update Layout.tsx

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -79,7 +79,7 @@ const Layout = () => {
                             aria-hidden="true"
                         />
                         <Link to="/" className={styles.headerTitleContainer}>
-                            <h1 className={styles.headerTitle}>White Cube Q&A</h1>
+                            <h1 className={styles.headerTitle}>White Cube QA</h1>
                         </Link>
                     </Stack>
                     <Stack horizontal tokens={{ childrenGap: 4 }}>
