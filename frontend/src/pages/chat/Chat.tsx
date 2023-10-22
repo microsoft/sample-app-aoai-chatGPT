a5e984813bddfb7bbd8f5e326e96b69b0f87f5e0
Lupael <lupael@i4e.com.bd>
2023-10-22 18:29:53 +0600

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -86,13 +86,8 @@ const Chat = () => {
     }
     
     const getUserInfoList = async () => {
-        const userInfoList = await getUserInfo();
-        if (userInfoList.length === 0 && window.location.hostname !== "127.0.0.1") {
-            setShowAuthMessage(true);
-        }
-        else {
-            setShowAuthMessage(false);
-        }
+        setShowAuthMessage(false);
+}
     }
 
     const makeApiRequestWithoutCosmosDB = async (question: string, conversationId?: string) => {
