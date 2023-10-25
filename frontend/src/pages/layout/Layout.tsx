6f2dc8d021ec31742a6ec7b61e22501062a795f6
whiseung <wsseo@cloocusc.com>
2023-10-25 10:36:09 +0900

    css test

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet, Link } from "react-router-dom";
 import styles from "./Layout.module.css";
 import Azure from "../../assets/Azure.svg";
+import Dongwon from "../../assets/dongwon.svg";
 import { CopyRegular, ShareRegular } from "@fluentui/react-icons";
 import { CommandBarButton, Dialog, Stack, TextField, ICommandBarStyles, IButtonStyles, DefaultButton  } from "@fluentui/react";
 import { useContext, useEffect, useState } from "react";
@@ -74,20 +75,20 @@ const Layout = () => {
                 >
                     <Stack horizontal verticalAlign="center">
                         <img
-                            src={Azure}
+                            src={Dongwon}
                             className={styles.headerIcon}
                             aria-hidden="true"
                         />
                         <Link to="/" className={styles.headerTitleContainer}>
-                            <h1 className={styles.headerTitle1}>Dongwon</h1>
+                            {/* <h1 className={styles.headerTitle1}>d</h1> */}
                         </Link>
                     </Stack>
-                    <Stack horizontal tokens={{ childrenGap: 4 }}>
+                    {/* <Stack horizontal tokens={{ childrenGap: 4 }}>
                             {(appStateContext?.state.isCosmosDBAvailable?.status !== CosmosDBStatus.NotConfigured) && 
                                 <HistoryButton onClick={handleHistoryClick} text={appStateContext?.state?.isChatHistoryOpen ? "Hide chat history" : "Show chat history"}/>    
                             }
                             <ShareButton onClick={handleShareClick} />
-                    </Stack>
+                    </Stack> */}
 
                 </Stack>
             </header>
