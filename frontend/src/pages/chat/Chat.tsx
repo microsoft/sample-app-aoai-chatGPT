a562eb98adc5039661febc865140ccfedcc8ee63
Ian Seabock (Centific Technologies Inc) <v-ianseabock@microsoft.com>
2023-11-07 15:38:35 -0800

    Disable chat input until CosmoDB is loaded if configured

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -87,7 +87,14 @@ const Chat = () => {
             setErrorMsg(null)
         }, 500);
     }
-    
+
+    useEffect(() => {
+        appStateContext?.state.chatHistoryLoadingState === ChatHistoryLoadingState.Loading
+            ? setIsLoading(true)
+            : setIsLoading(false);
+
+    }, [appStateContext?.state.chatHistoryLoadingState])
+
     const getUserInfoList = async () => {
         const userInfoList = await getUserInfo();
         if (userInfoList.length === 0 && window.location.hostname !== "127.0.0.1") {
