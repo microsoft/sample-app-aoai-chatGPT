64a1df1737cf202d317d0d4c7814ceb933959486
Christoph Goessmann <mail@goessmann.io>
2023-12-15 21:10:54 +0100

    Remove unused AAD token
    Requiring the AAD token produces error when using other identity providers, such as those using OpenID. Moreover, the AAD token (X-Ms-Token-Aad-Id-Token) is not used anywhere in the app.

--- backend/auth/auth_utils.py
+++ backend/auth/auth_utils.py
@@ -13,8 +13,6 @@ def get_authenticated_user_details(request_headers):
     user_object['user_principal_id'] = raw_user_object['X-Ms-Client-Principal-Id']
     user_object['user_name'] = raw_user_object['X-Ms-Client-Principal-Name']
     user_object['auth_provider'] = raw_user_object['X-Ms-Client-Principal-Idp']
-    user_object['auth_token'] = raw_user_object['X-Ms-Token-Aad-Id-Token']
     user_object['client_principal_b64'] = raw_user_object['X-Ms-Client-Principal']
-    user_object['aad_id_token'] = raw_user_object["X-Ms-Token-Aad-Id-Token"]
 
     return user_object
