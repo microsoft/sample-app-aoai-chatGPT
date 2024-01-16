8f197f45f647d58908445a58a54cbe8f380ce43b
adminContosoTestAL <127731807+adminContosoTestAL@users.noreply.github.com>
2024-01-16 16:56:03 +0200

    Update Chat.tsx new logo

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -9,7 +9,7 @@ import uuid from 'react-uuid';
 import { isEmpty } from "lodash-es";
 
 import styles from "./Chat.module.css";
-import Contoso from "../../assets/Contoso.svg";
+import Contoso from "../../assets/Alex.png";
 
 import {
     ChatMessage,
