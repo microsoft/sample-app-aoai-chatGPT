0084fe214a336ea8b78e082225a9ea7822dc8d3b
nicolamoresi <119077932+nicolamoresi@users.noreply.github.com>
2024-01-16 14:54:47 +0100

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -590,8 +590,8 @@ const Chat = () => {
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>Start chatting</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your questions</h2>
+                                <h1 className={styles.chatEmptyStateTitle}>Inizia la conversazione</h1>
+                                <h2 className={styles.chatEmptyStateSubtitle}>Le risposte verranno prese dai documentiPDF  forniti.</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px" }} role="log">
