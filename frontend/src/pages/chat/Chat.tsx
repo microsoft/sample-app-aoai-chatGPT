a35c49021632b019f2034612d6d4b3b765b9b5e9
nicolamoresi <119077932+nicolamoresi@users.noreply.github.com>
2024-01-16 15:07:04 +0100

    Update Chat.tsx1

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -10,6 +10,7 @@ import { isEmpty } from "lodash-es";
 
 import styles from "./Chat.module.css";
 import Contoso from "../../assets/Contoso.svg";
+import Azure from "../../assets/Azure.svg";
 
 import {
     ChatMessage,
@@ -586,7 +587,7 @@ const Chat = () => {
                         {!messages || messages.length < 1 ? (
                             <Stack className={styles.chatEmptyState}>
                                 <img
-                                    src={Contoso}
+                                    src={Azure}
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
