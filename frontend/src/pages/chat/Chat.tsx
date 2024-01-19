3261cd5c6a8c7bec72d58bed10db2c75c1102dd3
Joe Childress (AIM Consulting Addison Group) <v-jchildress@microsoft.com>
2024-01-19 13:20:46 -0800

    undoing change intended for testing only

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -95,7 +95,7 @@ const Chat = () => {
 
     const getUserInfoList = async () => {
       const userInfoList = await getUserInfo();
-      if (userInfoList.length === 0 && window.location.hostname == "127.0.0.1") {
+      if (userInfoList.length === 0 && window.location.hostname !== "127.0.0.1") {
         setShowAuthMessage(true);
       }
     }
