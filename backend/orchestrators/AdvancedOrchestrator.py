b934ba9adc30dc5832c1b11e42e0c9f573f171cf
Bryan Scheurman <brysc@microsoft.com>
2024-01-19 09:16:11 -0800

    Added custom orchestrator with ability dynamically select an AOAI resource/endpoint per request

--- backend/orchestrators/AdvancedOrchestrator.py
+++ backend/orchestrators/AdvancedOrchestrator.py
@@ -1,15 +1,39 @@
-from .Orchestrator import Orchestrator
 import requests
+import copy
+import json
+import logging
 import openai
-from flask import  Response, request, jsonify
+from flask import Response, request, jsonify
+from .Orchestrator import Orchestrator
 
-class DefaultOrchestrator(Orchestrator):
+class OpenAIContext():
+    def __init__(self, resource: str, model: str, endpoint: str, key: str, version: str):
+        self.resource = resource
+        self.model = model
+        self.endpoint = endpoint
+        self.key = key
+        self.version = version    
+
+class AdvancedOrchestrator(Orchestrator):
+    def get_openai_context(self) -> OpenAIContext:
+        # TODO: Add logic to load balance between multiple OpenAI endpoints
+        # For now just configuring with the same endpoint that DefaultOrchestrator uses
+        return OpenAIContext(
+            resource = super().AZURE_OPENAI_RESOURCE,
+            model = super().AZURE_OPENAI_MODEL,
+            endpoint = super().AZURE_OPENAI_ENDPOINT,
+            key = super().AZURE_OPENAI_KEY,
+            version = super().AZURE_OPENAI_PREVIEW_API_VERSION
+        )
+        
     # Post chat info if data configured
     def conversation_with_data(self, request_body, message_uuid):
+        openai_context = self.get_openai_context()
+
         # Set up request variables
-        body, headers = super().prepare_body_headers_with_data(request)
-        base_url = super().AZURE_OPENAI_ENDPOINT if super().AZURE_OPENAI_ENDPOINT else f"https://{super().AZURE_OPENAI_RESOURCE}.openai.azure.com/"
-        endpoint = f"{base_url}openai/deployments/{super().AZURE_OPENAI_MODEL}/extensions/chat/completions?api-version={super().AZURE_OPENAI_PREVIEW_API_VERSION}"
+        body, headers = self.prepare_body_headers_with_data(request, openai_context)
+        base_url = openai_context.endpoint if openai_context.endpoint else f"https://{openai_context.resource}.openai.azure.com/"
+        endpoint = f"{base_url}openai/deployments/{openai_context.model}/extensions/chat/completions?api-version={openai_context.version}"
         history_metadata = request_body.get("history_metadata", {})
 
         # Return response if streaming is not enabled
@@ -33,6 +57,10 @@ class DefaultOrchestrator(Orchestrator):
 
     # Post chat info if data not configured
     def conversation_without_data(self, request_body, message_uuid):
+        openai_context = self.get_openai_context()
+
+        # TODO: Need to pass the context into the *args of the .create call below instead of using the global variables
+
         # Setup for direct query to OpenAI
         openai.api_type = "azure"
         openai.api_base = super().AZURE_OPENAI_ENDPOINT if super().AZURE_OPENAI_ENDPOINT else f"https://{super().AZURE_OPENAI_RESOURCE}.openai.azure.com/"
@@ -89,3 +117,153 @@ class DefaultOrchestrator(Orchestrator):
         # Format and return response if streaming is enabled
         else:
             return Response(super().stream_without_data(response, history_metadata, message_uuid), mimetype='text/event-stream')
+        
+    # Format request body and headers with relevant info based on search type
+    def prepare_body_headers_with_data(self, request, openai_context: OpenAIContext):
+        request_messages = request.json["messages"]
+
+        body = {
+            "messages": request_messages,
+            "temperature": float(super().AZURE_OPENAI_TEMPERATURE),
+            "max_tokens": int(super().AZURE_OPENAI_MAX_TOKENS),
+            "top_p": float(super().AZURE_OPENAI_TOP_P),
+            "stop": super().AZURE_OPENAI_STOP_SEQUENCE.split("|") if super().AZURE_OPENAI_STOP_SEQUENCE else None,
+            "stream": super().SHOULD_STREAM,
+            "dataSources": []
+        }
+
+        if super().DATASOURCE_TYPE == "AzureCognitiveSearch":
+            # Set query type
+            query_type = "simple"
+            if super().AZURE_SEARCH_QUERY_TYPE:
+                query_type = super().AZURE_SEARCH_QUERY_TYPE
+            elif super().AZURE_SEARCH_USE_SEMANTIC_SEARCH.lower() == "true" and super().AZURE_SEARCH_SEMANTIC_SEARCH_CONFIG:
+                query_type = "semantic"
+
+            # Set filter
+            filter = None
+            userToken = None
+            if super().AZURE_SEARCH_PERMITTED_GROUPS_COLUMN:
+                userToken = request.headers.get('X-MS-TOKEN-AAD-ACCESS-TOKEN', "")
+                if super().DEBUG_LOGGING:
+                    logging.debug(f"USER TOKEN is {'present' if userToken else 'not present'}")
+
+                filter = super().generateFilterString(userToken)
+                if super().DEBUG_LOGGING:
+                    logging.debug(f"FILTER: {filter}")
+
+            body["dataSources"].append(
+                {
+                    "type": "AzureCognitiveSearch",
+                    "parameters": {
+                        "endpoint": f"https://{super().AZURE_SEARCH_SERVICE}.search.windows.net",
+                        "key": super().AZURE_SEARCH_KEY,
+                        "indexName": super().AZURE_SEARCH_INDEX,
+                        "fieldsMapping": {
+                            "contentFields": super().parse_multi_columns(super().AZURE_SEARCH_CONTENT_COLUMNS) if super().AZURE_SEARCH_CONTENT_COLUMNS else [],
+                            "titleField": super().AZURE_SEARCH_TITLE_COLUMN if super().AZURE_SEARCH_TITLE_COLUMN else None,
+                            "urlField": super().AZURE_SEARCH_URL_COLUMN if super().AZURE_SEARCH_URL_COLUMN else None,
+                            "filepathField": super().AZURE_SEARCH_FILENAME_COLUMN if super().AZURE_SEARCH_FILENAME_COLUMN else None,
+                            "vectorFields": super().parse_multi_columns(super().AZURE_SEARCH_VECTOR_COLUMNS) if super().AZURE_SEARCH_VECTOR_COLUMNS else []
+                        },
+                        "inScope": True if super().AZURE_SEARCH_ENABLE_IN_DOMAIN.lower() == "true" else False,
+                        "topNDocuments": int(super().AZURE_SEARCH_TOP_K),
+                        "queryType": query_type,
+                        "semanticConfiguration": super().AZURE_SEARCH_SEMANTIC_SEARCH_CONFIG if super().AZURE_SEARCH_SEMANTIC_SEARCH_CONFIG else "",
+                        "roleInformation": super().AZURE_OPENAI_SYSTEM_MESSAGE,
+                        "filter": filter,
+                        "strictness": int(super().AZURE_SEARCH_STRICTNESS)
+                    }
+                })
+        elif super().DATASOURCE_TYPE == "AzureCosmosDB":
+            # Set query type
+            query_type = "vector"
+
+            body["dataSources"].append(
+                {
+                    "type": "AzureCosmosDB",
+                    "parameters": {
+                        "connectionString": super().AZURE_COSMOSDB_MONGO_VCORE_CONNECTION_STRING,
+                        "indexName": super().AZURE_COSMOSDB_MONGO_VCORE_INDEX,
+                        "databaseName": super().AZURE_COSMOSDB_MONGO_VCORE_DATABASE,
+                        "containerName": super().AZURE_COSMOSDB_MONGO_VCORE_CONTAINER,                    
+                        "fieldsMapping": {
+                            "contentFields": super().parse_multi_columns(super().AZURE_COSMOSDB_MONGO_VCORE_CONTENT_COLUMNS) if super().AZURE_COSMOSDB_MONGO_VCORE_CONTENT_COLUMNS else [],
+                            "titleField": super().AZURE_COSMOSDB_MONGO_VCORE_TITLE_COLUMN if super().AZURE_COSMOSDB_MONGO_VCORE_TITLE_COLUMN else None,
+                            "urlField": super().AZURE_COSMOSDB_MONGO_VCORE_URL_COLUMN if super().AZURE_COSMOSDB_MONGO_VCORE_URL_COLUMN else None,
+                            "filepathField": super().AZURE_COSMOSDB_MONGO_VCORE_FILENAME_COLUMN if super().AZURE_COSMOSDB_MONGO_VCORE_FILENAME_COLUMN else None,
+                            "vectorFields": super().parse_multi_columns(super().AZURE_COSMOSDB_MONGO_VCORE_VECTOR_COLUMNS) if super().AZURE_COSMOSDB_MONGO_VCORE_VECTOR_COLUMNS else []
+                        },
+                        "inScope": True if super().AZURE_COSMOSDB_MONGO_VCORE_ENABLE_IN_DOMAIN.lower() == "true" else False,
+                        "topNDocuments": int(super().AZURE_COSMOSDB_MONGO_VCORE_TOP_K),
+                        "strictness": int(super().AZURE_COSMOSDB_MONGO_VCORE_STRICTNESS),
+                        "queryType": query_type,
+                        "roleInformation": super().AZURE_OPENAI_SYSTEM_MESSAGE
+                    }
+                }
+            )
+
+        elif super().DATASOURCE_TYPE == "Elasticsearch":
+            body["dataSources"].append(
+                {
+                    "messages": request_messages,
+                    "temperature": float(super().AZURE_OPENAI_TEMPERATURE),
+                    "max_tokens": int(super().AZURE_OPENAI_MAX_TOKENS),
+                    "top_p": float(super().AZURE_OPENAI_TOP_P),
+                    "stop": super().AZURE_OPENAI_STOP_SEQUENCE.split("|") if super().AZURE_OPENAI_STOP_SEQUENCE else None,
+                    "stream": super().SHOULD_STREAM,
+                    "dataSources": [
+                        {
+                            "type": "AzureCognitiveSearch",
+                            "parameters": {
+                                "endpoint": super().ELASTICSEARCH_ENDPOINT,
+                                "encodedApiKey": super().ELASTICSEARCH_ENCODED_API_KEY,
+                                "indexName": super().ELASTICSEARCH_INDEX,
+                                "fieldsMapping": {
+                                    "contentFields": super().parse_multi_columns(super().ELASTICSEARCH_CONTENT_COLUMNS) if super().ELASTICSEARCH_CONTENT_COLUMNS else [],
+                                    "titleField": super().ELASTICSEARCH_TITLE_COLUMN if super().ELASTICSEARCH_TITLE_COLUMN else None,
+                                    "urlField": super().ELASTICSEARCH_URL_COLUMN if super().ELASTICSEARCH_URL_COLUMN else None,
+                                    "filepathField": super().ELASTICSEARCH_FILENAME_COLUMN if super().ELASTICSEARCH_FILENAME_COLUMN else None,
+                                    "vectorFields": super().parse_multi_columns(super().ELASTICSEARCH_VECTOR_COLUMNS) if super().ELASTICSEARCH_VECTOR_COLUMNS else []
+                                },
+                                "inScope": True if super().ELASTICSEARCH_ENABLE_IN_DOMAIN.lower() == "true" else False,
+                                "topNDocuments": int(super().ELASTICSEARCH_TOP_K),
+                                "queryType": super().ELASTICSEARCH_QUERY_TYPE,
+                                "roleInformation": super().AZURE_OPENAI_SYSTEM_MESSAGE,
+                                "embeddingEndpoint": super().AZURE_OPENAI_EMBEDDING_ENDPOINT,
+                                "embeddingKey": super().AZURE_OPENAI_EMBEDDING_KEY,
+                                "embeddingModelId": super().ELASTICSEARCH_EMBEDDING_MODEL_ID,
+                                "strictness": int(super().ELASTICSEARCH_STRICTNESS)
+                            }
+                        }
+                    ]
+                }
+            )
+        else:
+            raise Exception(f"DATASOURCE_TYPE is not configured or unknown: {super().DATASOURCE_TYPE}")
+
+        if "vector" in query_type.lower():
+            if super().AZURE_OPENAI_EMBEDDING_NAME:
+                body["dataSources"][0]["parameters"]["embeddingDeploymentName"] = super().AZURE_OPENAI_EMBEDDING_NAME
+            else:
+                body["dataSources"][0]["parameters"]["embeddingEndpoint"] = super().AZURE_OPENAI_EMBEDDING_ENDPOINT
+                body["dataSources"][0]["parameters"]["embeddingKey"] = super().AZURE_OPENAI_EMBEDDING_KEY
+
+        if super().DEBUG_LOGGING:
+            body_clean = copy.deepcopy(body)
+            if body_clean["dataSources"][0]["parameters"].get("key"):
+                body_clean["dataSources"][0]["parameters"]["key"] = "*****"
+            if body_clean["dataSources"][0]["parameters"].get("connectionString"):
+                body_clean["dataSources"][0]["parameters"]["connectionString"] = "*****"
+            if body_clean["dataSources"][0]["parameters"].get("embeddingKey"):
+                body_clean["dataSources"][0]["parameters"]["embeddingKey"] = "*****"
+                
+            logging.debug(f"REQUEST BODY: {json.dumps(body_clean, indent=4)}")
+
+        headers = {
+            'Content-Type': 'application/json',
+            'api-key': openai_context.key,
+            "x-ms-useragent": "GitHubSampleWebApp/PublicAPI/3.0.0"
+        }
+
+        return body, headers
