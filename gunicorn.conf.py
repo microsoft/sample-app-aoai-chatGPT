388e9110a070726ff11f0980e870406fd3cc770d
Sarah Widder <sawidder@microsoft.com>
2024-01-19 17:11:15 -0800

    Add gunicorn config

---
+++ gunicorn.conf.py
@@ -0,0 +1,9 @@
+import multiprocessing
+
+bind = "0.0.0.0"
+
+timeout = 230
+# https://learn.microsoft.com/en-us/troubleshoot/azure/app-service/web-apps-performance-faqs#why-does-my-request-time-out-after-230-seconds
+
+num_cpus = multiprocessing.cpu_count()
+workers = (num_cpus * 2) + 1
