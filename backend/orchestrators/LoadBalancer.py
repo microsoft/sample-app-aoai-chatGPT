2e8fc8afeae119da2e9817ffcf6adce1d84516ca
Chana <v-chanacohn@microsoft.com>
2024-01-23 08:43:05 -0800

    logging selected context

--- backend/orchestrators/LoadBalancer.py
+++ backend/orchestrators/LoadBalancer.py
@@ -1,6 +1,7 @@
 import random
 import os
 import json
+import logging
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -27,6 +28,7 @@ class OpenAIContext():
 
 class LoadBalancer:  
     def __init__(self):
+
         open_ai_env_contexts = os.environ.get("AZURE_OPENAI_CONTEXTS")
         open_ai_contexts = json.loads(open_ai_env_contexts)
         api_version = os.environ.get("AZURE_OPENAI_PREVIEW_API_VERSION")
@@ -43,11 +45,15 @@ class LoadBalancer:
         self.all_weights = [round(sum(relative_weights[:i+1]), 2) for i in range(len(relative_weights))]
 
     def get_openai_context(self) -> OpenAIContext:
-
+        DEBUG = os.environ.get("DEBUG", "false")
+        DEBUG_LOGGING = DEBUG.lower() == "true"
         for _ in enumerate(self.contexts):
             rand_num = random.random()  # Generates a random float number between 0.0 to 1.0
 
             for i, cumulative_weight in enumerate(self.all_weights):
                 if rand_num <= cumulative_weight:
                     selected_context = self.contexts[i].to_dict()
+                    print(f"print Selected context: {selected_context}")
+                    if DEBUG_LOGGING:
+                        logging.debug(f"debug Selected context: {selected_context}")
                     return selected_context
