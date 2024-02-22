fa1dc60c2e9388ec928ee47efd99ed9a6c7990ba
wlonggy88 <139519015+wlonggy88@users.noreply.github.com>
2024-02-22 16:54:10 +0800

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -10,7 +10,7 @@ import { isEmpty } from "lodash-es";
 import DOMPurify from 'dompurify';
 
 import styles from "./Chat.module.css";
-import Contoso from "../../assets/Contoso.svg";
+import Contoso from "../../assets/Contos.svg";
 import { XSSAllowTags } from "../../constants/xssAllowTags";
 
 import {
