75f3355ae99467ed4a943fb392c8104750167f78
Kanagala, Suresh <KanagalaS@bv.com>
2024-03-18 19:37:57 +0000

    changed title color

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ from backend.utils import format_as_ndjson, format_stream_response, generateFilt
 bp = Blueprint("routes", __name__, static_folder="static", template_folder="static")
 
 # UI configuration (optional)
-UI_TITLE = os.environ.get("UI_TITLE") or " "
+UI_TITLE = os.environ.get("UI_TITLE") or "Home"
 UI_LOGO = os.environ.get("UI_LOGO")
 UI_CHAT_LOGO = os.environ.get("UI_CHAT_LOGO")
 UI_CHAT_TITLE = os.environ.get("UI_CHAT_TITLE") or "Start chatting"
