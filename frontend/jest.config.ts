b4824ebfdc7a175c7cd31914eb5709bc0cf1f185
Ian Seabock (Centific Technologies Inc) <v-ianseabock@microsoft.com>
2024-04-18 09:36:59 -0700

    polyfill test

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -5,6 +5,6 @@ const config: Config.InitialOptions = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-  setupFilesAfterEnv: ['./setup-jest.js'],
+  setupFilesAfterEnv: ['"<rootDir>/jestSetup.js"'],
 };
 export default config;
