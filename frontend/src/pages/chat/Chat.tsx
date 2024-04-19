c61d982e7743ca4f5b27a0bc8c34af84c8279bd8
vaish9102000 <56046613+vaish9102000@users.noreply.github.com>
2024-04-19 13:08:04 +0530

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -649,12 +649,12 @@ const Chat = () => {
                         {!messages || messages.length < 1 ? (
                             <Stack className={styles.chatEmptyState}>
                                 <img
-                                    src={ui?.chat_logo ? ui.chat_logo : Contoso}
+                                    src={ui?.chat_logo ? ui.chat_logo : ElasticOps-AI}
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>{ui?.chat_title}</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>{ui?.chat_description}</h2>
+                                <h1 className={styles.chatEmptyStateTitle}>ElasticOps Chat AI</h1>
+                                <h2 className={styles.chatEmptyStateSubtitle}>Start your quires</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px" }} role="log">
