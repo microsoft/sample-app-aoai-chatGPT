0163710e8ac4a9c52802cd9240284ac601d64ac3
Anil Kumar <anilkumar@bloominbrands.com>
2024-04-24 18:30:04 +0530

    Menu Change for Link

--- frontend/src/pages/sidebar/Sidebar.tsx
+++ frontend/src/pages/sidebar/Sidebar.tsx
@@ -9,10 +9,10 @@ const Sidebar = () => {
                 <Link to="https://ai-dev.bv.com" className={styles.headerTitleContainer}>
                     <h1 className={styles.headerTitle}>Home</h1>
                 </Link>
-                 <Link to="https://ai-dev.bv.com" className={styles.headerTitleContainer}>
+                 <Link to="https://360.articulate.com/review/content/0618b59e-d3d7-4516-b76c-738614b1fa88/review" className={styles.headerTitleContainer}>
                     <h1 className={styles.headerTitle}>Training</h1>
                 </Link>
-                <Link to="https://ai-dev.bv.com" className={styles.headerTitleContainer}>
+                <Link to="https://blackandveatch.sharepoint.com/sites/BV/Corp/CIO/SitePages/ChatGPT.aspx?referrer=Yammer&referrerScenario=Feed.View" className={styles.headerTitleContainer}>
                     <h1 className={styles.headerTitle}>Resources</h1>
                 </Link>
             </Stack>
