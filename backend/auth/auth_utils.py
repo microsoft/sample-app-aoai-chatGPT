d0f020b08b52fc83b6346204000d04a1fc28fb3e
slreznit <slreznit@microsoft.com>
2024-05-07 11:15:49 +0000

    Add ms_defemder_utils

--- backend/auth/auth_utils.py
+++ backend/auth/auth_utils.py
@@ -1,7 +1,3 @@
-import base64
-import json
-import logging
-
 def get_authenticated_user_details(request_headers):
     user_object = {}
 
@@ -22,18 +18,3 @@ def get_authenticated_user_details(request_headers):
     user_object['aad_id_token'] = raw_user_object.get('X-Ms-Token-Aad-Id-Token')
 
     return user_object
-
-def get_tenantid(client_principal_b64):
-    tenant_id = ''
-    if client_principal_b64:   
-        try:
-            # Decode the base64 header to get the JSON string
-            decoded_bytes = base64.b64decode(client_principal_b64)
-            decoded_string = decoded_bytes.decode('utf-8')
-            # Convert the JSON string1into a Python dictionary
-            user_info = json.loads(decoded_string)
-            # Extract the tenant ID
-            tenant_id = user_info.get('tid')  # 'tid' typically holds the tenant ID
-        except Exception as ex:
-            logging.exception(ex)
-    return tenant_id
