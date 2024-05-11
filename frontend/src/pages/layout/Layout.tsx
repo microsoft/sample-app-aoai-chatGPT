3b487682aed2b50cfaa61ab49658ce83de710bce
Mitesh Patel <129920462+MiteshNYC@users.noreply.github.com>
2024-05-11 13:46:53 -0400

    Update Layout.tsx

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -73,7 +73,7 @@ const Layout = () => {
           <Stack horizontal verticalAlign="center">
             <img src={ui?.logo ? ui.logo : Contoso} className={styles.headerIcon} aria-hidden="true" alt="" />
             <Link to="/" className={styles.headerTitleContainer}>
-              <h1 className={styles.headerTitle}>{ui?.title}</h1>
+              <h1 className={styles.headerTitle}>Davis Polk</h1>
             </Link>
           </Stack>
           <Stack horizontal tokens={{ childrenGap: 4 }} className={styles.shareButtonContainer}>
