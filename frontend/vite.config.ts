667ec01d4c846d29a6b8806c27cbddd7b1cae6e6
basspro-asif <araza@basspro.com>
2024-06-12 18:39:24 +0500

    update image v5

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/",
   plugins: [react()],
   build: {
-    outDir: 'build',
+    outDir: '../static',
     emptyOutDir: true,
     sourcemap: true
   },
