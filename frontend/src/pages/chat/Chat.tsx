534a4668b933be742159654e7c45c06793e6a583
Ian Seabock (Centific Technologies Inc) <v-ianseabock@microsoft.com>
2024-06-12 12:12:53 -0700

    Hide Stop Generating button when no messages

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -825,7 +825,7 @@ const Chat = () => {
             )}
 
             <Stack horizontal className={styles.chatInput}>
-              {isLoading && (
+              {isLoading && messages.length > 0 && (
                 <Stack
                   horizontal
                   className={styles.stopGeneratingContainer}
