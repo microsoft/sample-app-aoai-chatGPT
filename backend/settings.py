36c422607723c48eea94834267a0be862956afe9
Maicon Stedille <maicon.stedille@outlook.com>
2024-06-16 12:09:46 -0300

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -41,7 +41,7 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "Contoso"
+    title: str = "Energy Telecom"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
     chat_title: str = "Start chatting"
