199706e468a9b24124d1bed20c6f3bf8c3871ad4
Jon Burchel <jburchel@microsoft.com>
2024-07-22 16:39:56 -0400

    Try again

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -94,7 +94,7 @@ const Chat = () => {
   const [queryParams, setQueryParams] = useState<string>("");
 
   useEffect(() => {
-    const params = [];
+    const params = string[];
     query.forEach((value, key) => {
       params.push(`${key}: ${value}`);
     });
