aa2fdf27d214fabad3d5afce7f94232687768550
Jon Burchel <jburchel@microsoft.com>
2024-07-28 15:24:33 -0400

    Try again

--- deploy.sh
+++ deploy.sh
@@ -22,28 +22,12 @@ fi
 source venv/scripts/activate
 
 # Check if python3 is available
-if ! command -v python3 &> /dev/null
+if ! command -v python3.12 &> /dev/null
 then
     echo "Python3 could not be found"
     exit 1
 fi
 
-# Check if python3-venv is available
-if ! python3 -m venv &> /dev/null
-then
-    echo "Installing python3-venv"
-    apt-get update
-    apt-get install -y python3-venv
-fi
-
-# Ensure pip is available
-if ! command -v pip &> /dev/null
-then
-    echo "Installing pip"
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python3 get-pip.py
-    rm get-pip.py
-fi
 
 # Install dependencies
 pip install --upgrade pip
