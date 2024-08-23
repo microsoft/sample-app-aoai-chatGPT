acc5895e0e31745b5f672a16b005967f6ac38e47
Carmen Sáez <carmen047839@gmail.com>
2024-08-23 14:53:34 -0400

    Chat Button per PDF
    SideBar was modified to trun the text, add ellipse and tooltip, also a button on each to start a chat for that pdf file.

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -5,6 +5,7 @@ import { MoonLoader } from 'react-spinners'
 import styles from '../Sidebar/Sidebar.module.css'
 import { pdfList } from '../../api'
 import PdfModal from '../PdfModal/PdfModal'
+import { IconButton, TooltipHost } from '@fluentui/react'
 
 interface SidebarMenuProps {
   collapsed: boolean
@@ -32,6 +33,11 @@ const SidebarMenu: FC<SidebarMenuProps> = ({
     setShowPdfModal(!showPdfModal)
   }
 
+  const handleButtonClick = (key: string) => {
+    console.log(`Button clicked for inititiating a new PDF chat: ${key}`);
+  }
+
+
   useEffect(() => {
     setLoading(true)
     const getPdfList = async () => {
@@ -56,8 +62,8 @@ const SidebarMenu: FC<SidebarMenuProps> = ({
     getPdfList()
   }, [])
 
-// Function Pdf Search
-useEffect(() => {
+  // Function Pdf Search
+  useEffect(() => {
     const filteredList = Object.entries(pdfListJson).filter(([key]) =>
       key.toLowerCase().includes(searchQuery.toLowerCase())
     );
@@ -78,32 +84,32 @@ useEffect(() => {
           rootStyles={
             collapsed && !toggled
               ? {
-                  ['.' + sidebarClasses.container]: {
-                    backgroundColor: 'rgb(202, 203, 219)',
-                    '&::-webkit-scrollbar': {
-                      display: 'none'
-                    }
+                ['.' + sidebarClasses.container]: {
+                  backgroundColor: 'rgb(202, 203, 219)',
+                  '&::-webkit-scrollbar': {
+                    display: 'none'
                   }
                 }
+              }
               : {
-                  ['.' + sidebarClasses.container]: {
-                    backgroundColor: 'rgb(202, 203, 219)',
-                    '&::-webkit-scrollbar': {
-                      width: '5px',
-                      height: '5px',
-                      backgroundColor: '#f5f5f5',
-                      borderRadius: '3px'
-                    },
-                    '&::-webkit-scrollbar-track': {
-                      height: '20px',
-                      marginBlock: '-3px'
-                    },
-                    '&::-webkit-scrollbar-thumb': {
-                      background: '#334768',
-                      borderRadius: '3px'
-                    }
+                ['.' + sidebarClasses.container]: {
+                  backgroundColor: 'rgb(202, 203, 219)',
+                  '&::-webkit-scrollbar': {
+                    width: '5px',
+                    height: '5px',
+                    backgroundColor: '#f5f5f5',
+                    borderRadius: '3px'
+                  },
+                  '&::-webkit-scrollbar-track': {
+                    height: '20px',
+                    marginBlock: '-3px'
+                  },
+                  '&::-webkit-scrollbar-thumb': {
+                    background: '#334768',
+                    borderRadius: '3px'
                   }
                 }
+              }
           }>
           <Menu>
             {toggled ? null : (
@@ -142,41 +148,77 @@ useEffect(() => {
             )}
             {Object.keys(filteredPdfList).length > 0 && loading === false
               ? Object.entries(filteredPdfList).map(([key, value]) => (
-                  <MenuItem
-                    style={collapsed && !toggled ? { visibility: 'hidden' } : { color: '#201F1E' }}
-                    rootStyles={{
-                      ['.' + menuClasses.label]: {
-                        whiteSpace: 'normal',
-                        maxWidth: '100%',
-                        wordWrap: 'break-word',
-                        overflow: 'visible',
-                      },
-                      ['.' + menuClasses.button]: {
-                        height: '100% !important',
-                        paddingTop: '10px',
-                        paddingBottom: '10px',
-                      },
-                    }}
-                    onClick={() => {
-                      setSelectedPdf({ name: key.toString(), url: value.toString() });
-                      setShowPdfModal(true);
-                    }}
-                  >
-                    {key}
-                  </MenuItem>
-                ))
-              : loading === false && (
-                  <div
-                    style={
-                      collapsed && !toggled
-                        ? { visibility: 'hidden' }
-                        : { display: 'flex', alignItems: 'center', marginTop: '75%' }
-                    }
-                  >
-                    <DatabaseSlash color="#334768" size={35} style={{ marginLeft: '8px' }} />
-                    <span style={{ fontSize: '24px', marginLeft: '5px' }}>No files available</span>
+                <MenuItem
+                  style={collapsed && !toggled ? { visibility: 'hidden' } : { color: '#201F1E' }}
+                  rootStyles={{
+                    ['.' + menuClasses.label]: {
+                      whiteSpace: 'normal',
+                      maxWidth: '100%',
+                      wordWrap: 'break-word',
+                      overflow: 'visible',
+                    },
+                    ['.' + menuClasses.button]: {
+                      height: '100% !important',
+                      paddingTop: '10px',
+                      paddingBottom: '10px',
+                    },
+                  }}
+                  onClick={() => {
+                    setSelectedPdf({ name: key.toString(), url: value.toString() });
+                    setShowPdfModal(true);
+                  }}
+                >
+                  <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
+                    <TooltipHost
+                      content={key}
+                      styles={{
+                        root: {
+                          display: 'inline-block',
+                          maxWidth: 'calc(100% - 40px)', // Keep this consistent with the span
+                          whiteSpace: 'nowrap',
+                          overflow: 'hidden',
+                          textOverflow: 'ellipsis',
+                        }
+                      }}
+                    >
+                      <span
+                        style={{
+                          whiteSpace: 'nowrap',
+                          overflow: 'hidden',
+                          textOverflow: 'ellipsis',
+                          display: 'inline-block',
+                          maxWidth: '100%',
+                        }}
+                      >
+                        {key}
+                      </span>
+                    </TooltipHost>
+                    <IconButton
+                      className={styles.itemChatButton}
+                      iconProps={{ iconName: 'CannedChat' }}
+                      title="Canned Chat"
+                      ariaLabel="Canned Chat"
+                      // styles={{ root: { color: '#0078d4' } }}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleButtonClick(key);
+                      }}
+                    />
                   </div>
-                )}
+                </MenuItem>
+              ))
+              : loading === false && (
+                <div
+                  style={
+                    collapsed && !toggled
+                      ? { visibility: 'hidden' }
+                      : { display: 'flex', alignItems: 'center', marginTop: '75%' }
+                  }
+                >
+                  <DatabaseSlash color="#334768" size={35} style={{ marginLeft: '8px' }} />
+                  <span style={{ fontSize: '24px', marginLeft: '5px' }}>No files available</span>
+                </div>
+              )}
             {loading && (
               <div className={styles.spinner}>
                 <MoonLoader size={45} color="#334768" loading={loading} speedMultiplier={0.8} />
