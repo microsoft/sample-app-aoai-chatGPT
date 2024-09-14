9c5e8d0d9f1a28e7c560984474ef9abd8781d524
Sanchit Aggarwal <Sanchit.Niec@gmail.com>
2024-09-14 16:33:14 +0530

    Initial Commit

--- backend/settings.py
+++ backend/settings.py
@@ -41,11 +41,11 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "Contoso"
+    title: str = "MSHack"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
-    chat_title: str = "Start chatting"
-    chat_description: str = "This chatbot is configured to answer your questions"
+    chat_title: str = "Start Hacking"
+    chat_description: str = "Caution: This can be addictive :)"
     favicon: str = "/favicon.ico"
     show_share_button: bool = True
     show_chat_history_button: bool = True
