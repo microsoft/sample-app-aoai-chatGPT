a49ed67b433c08ea117677e42283c49815208315
Tolga Yiğit <tolga.yig@gmail.com>
2024-09-25 13:30:05 +0300

    Update application title in settings.py to "Güntner GPT Unit"

--- backend/settings.py
+++ backend/settings.py
@@ -41,7 +41,7 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "Guentner AI Assistant"
+    title: str = "Güntner GPT Unit"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
     chat_title: str = "Start chatting"
