59722494b0b84a8e24034b882e60c5c3172e94a8
Gonçalo Ventura <goventur@microsoft.com>
2024-10-12 22:36:02 +0100

    Code quality improvements. Added documentation to README file.

--- app.py
+++ app.py
@@ -412,7 +412,7 @@ def process_function_call(response):
         return messages
     
     return None
-    
+
 async def send_chat_request(request_body, request_headers):
     filtered_messages = []
     messages = request_body.get("messages", [])
@@ -462,92 +462,98 @@ async def complete_chat_request(request_body, request_headers):
 
     return non_streaming_response
 
+class AzureOpenaiFunctionCallStreamState():
+    def __init__(self):
+        self.tool_calls = []
+        self.current_tool_call = None
+        self.tool_arguments_stream = ""
+        self.function_messages = []
+        self.tool_name = ""
+        self.tool_call_streaming_state = "INITIAL"
+
+
+def process_function_call_stream(completionChunk, function_call_stream_state, request_body, request_headers, history_metadata, apim_request_id):
+    if hasattr(completionChunk, "choices") and len(completionChunk.choices) > 0:
+        response_message = completionChunk.choices[0].delta
+        
+        # Function calling stream processing
+        if response_message.tool_calls and function_call_stream_state.tool_call_streaming_state in ["INITIAL", "STREAMING"]:
+            function_call_stream_state.tool_call_streaming_state = "STREAMING"
+            for tool_call_chunk in response_message.tool_calls:
+                # New tool call
+                if tool_call_chunk.id:
+                    if function_call_stream_state.current_tool_call:
+                        function_call_stream_state.tool_arguments_stream += tool_call_chunk.function.arguments if tool_call_chunk.function.arguments else ""
+                        function_call_stream_state.current_tool_call["tool_arguments"] = function_call_stream_state.tool_arguments_stream
+                        function_call_stream_state.tool_arguments_stream = ""
+                        function_call_stream_state.tool_name = ""
+                        function_call_stream_state.tool_calls.append(function_call_stream_state.current_tool_call)
+
+                    function_call_stream_state.current_tool_call = {
+                        "tool_id": tool_call_chunk.id,
+                        "tool_name": tool_call_chunk.function.name if function_call_stream_state.tool_name == "" else function_call_stream_state.tool_name
+                    }
+                else:
+                    function_call_stream_state.tool_arguments_stream += tool_call_chunk.function.arguments if tool_call_chunk.function.arguments else ""
+                
+        # Function call - Streaming completed
+        elif response_message.tool_calls is None and function_call_stream_state.tool_call_streaming_state == "STREAMING":
+            function_call_stream_state.current_tool_call["tool_arguments"] = function_call_stream_state.tool_arguments_stream
+            function_call_stream_state.tool_calls.append(function_call_stream_state.current_tool_call)
+            
+            for tool_call in function_call_stream_state.tool_calls:
+                tool_response = openai_remote_azure_function_call(tool_call["tool_name"], tool_call["tool_arguments"])
+
+                function_call_stream_state.function_messages.append({
+                    "role": "assistant",
+                    "function_call": {
+                        "name" : tool_call["tool_name"],
+                        "arguments": tool_call["tool_arguments"]
+                    },
+                    "content": None
+                })
+                function_call_stream_state.function_messages.append({
+                    "tool_call_id": tool_call["tool_id"],
+                    "role": "function",
+                    "name": tool_call["tool_name"],
+                    "content": tool_response,
+                })
+            
+            function_call_stream_state.tool_call_streaming_state = "COMPLETED"
+            return function_call_stream_state.tool_call_streaming_state
+        
+        else:
+            return function_call_stream_state.tool_call_streaming_state
+
 
 async def stream_chat_request(request_body, request_headers):
     response, apim_request_id = await send_chat_request(request_body, request_headers)
     history_metadata = request_body.get("history_metadata", {})
     
-    messages = []
-
     async def generate(apim_request_id, history_metadata):
-        tool_calls = []
-        current_tool_call = None
-        tool_arguments_stream = ""
-        function_messages = []
-        tool_name = ""
-        tool_call_streaming_state = "INITIAL"
+        if app_settings.azure_openai.function_call_azure_functions_enabled:
+            # Maintain state during function call streaming
+            function_call_stream_state = AzureOpenaiFunctionCallStreamState()
+            
+            async for completionChunk in response:
+                stream_state = process_function_call_stream(completionChunk, function_call_stream_state, request_body, request_headers, history_metadata, apim_request_id)
+                
+                # No function call, asistant response
+                if stream_state == "INITIAL":
+                    yield format_stream_response(completionChunk, history_metadata, apim_request_id)
 
-        async for completionChunk in response:
-            if app_settings.azure_openai.function_call_azure_functions_enabled:
-                if hasattr(completionChunk, "choices") and len(completionChunk.choices) > 0:
-                    response_message = completionChunk.choices[0].delta
-
-                    # Function calling stream processing
-                    if response_message.tool_calls and tool_call_streaming_state in ["INITIAL", "STREAMING"]:
-                        tool_call_streaming_state = "STREAMING"
-                        for tool_call_chunk in response_message.tool_calls:
-                            # New tool call
-                            if tool_call_chunk.id:
-                                if current_tool_call:
-                                    tool_arguments_stream += tool_call_chunk.function.arguments if tool_call_chunk.function.arguments else ""
-                                    current_tool_call["tool_arguments"] = tool_arguments_stream
-                                    tool_arguments_stream = ""
-                                    tool_name = ""
-                                    tool_calls.append(current_tool_call)
-
-                                current_tool_call = {
-                                    "tool_id": tool_call_chunk.id,
-                                    "tool_name": tool_call_chunk.function.name if tool_name == "" else tool_name
-                                }
-                            else:
-                                tool_arguments_stream += tool_call_chunk.function.arguments if tool_call_chunk.function.arguments else ""
-                            
-                    # Function call - Streaming completed
-                    elif response_message.tool_calls is None and tool_call_streaming_state == "STREAMING":
-                        current_tool_call["tool_arguments"] = tool_arguments_stream
-                        tool_calls.append(current_tool_call)
-                        
-                        for tool_call in tool_calls:
-                            tool_response = openai_remote_azure_function_call(tool_call["tool_name"], tool_call["tool_arguments"])
-
-                            function_messages.append({
-                                "role": "assistant",
-                                "function_call": {
-                                    "name" : tool_call["tool_name"],
-                                    "arguments": tool_call["tool_arguments"]
-                                },
-                                "content": None
-                            })
-                            function_messages.append({
-                                "tool_call_id": tool_call["tool_id"],
-                                "role": "function",
-                                "name": tool_call["tool_name"],
-                                "content": tool_response,
-                            })
-                        
-                        # Reset for the next tool call
-                        messages = function_messages
-                        function_messages = []
-                        tool_calls = []
-                        current_tool_call = None
-                        tool_arguments_stream = ""
-                        tool_name = ""
-                        tool_id = None
-                        tool_call_streaming_state = "COMPLETED"
-
-                        request_body["messages"].extend(messages)
-
-                        function_response, apim_request_id = await send_chat_request(request_body, request_headers)
-
-                        async for functionCompletionChunk in function_response:
-                            yield format_stream_response(functionCompletionChunk, history_metadata, apim_request_id)
-
-                    else:
-                        # No function call, asistant response
-                        yield format_stream_response(completionChunk, history_metadata, apim_request_id)
-    
-            else:
+                # Function call stream completed, functions were executed.
+                # Append function calls and results to history and send to OpenAI, to stream the final answer.
+                if stream_state == "COMPLETED":
+                    request_body["messages"].extend(function_call_stream_state.function_messages)
+                    function_response, apim_request_id = await send_chat_request(request_body, request_headers)
+                    async for functionCompletionChunk in function_response:
+                        yield format_stream_response(functionCompletionChunk, history_metadata, apim_request_id)
+                
+        else:
+            async for completionChunk in response:
                 yield format_stream_response(completionChunk, history_metadata, apim_request_id)
+
     return generate(apim_request_id=apim_request_id, history_metadata=history_metadata)
 
 
