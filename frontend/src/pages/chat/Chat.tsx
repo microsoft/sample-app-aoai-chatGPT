d6aee2c561047290fefc5c37e4dc0e282fed36bc
Everett Southwick <everett@southwick.dev>
2024-10-24 23:03:35 -0400

    chore(settings): allow empty values

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -833,7 +833,7 @@ const Chat = () => {
               <Stack className={styles.chatEmptyState}>
                 <img src={logo} className={styles.chatIcon} aria-hidden="true" />
                 {ui?.chat_title && <h1 className={styles.chatEmptyStateTitle}>{ui.chat_title}</h1>}
-                <h2 className={styles.chatEmptyStateSubtitle}>{ui?.chat_description}</h2>
+                {ui?.chat_description && <h2 className={styles.chatEmptyStateSubtitle}>{ui.chat_description}</h2>}
               </Stack>
             ) : (
               <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? '40px' : '0px' }} role="log">
