e774896b5df2e5c0fab7b3711ce4df5eac74d55e
AW101607 <2026-albertwijaya@kia-china.org>
2024-11-07 17:06:42 +0800

    Update Layout.tsx

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -104,7 +104,7 @@ const Layout = () => {
               selectors: {
                 ['@media (min-width: 480px)']: {
                   maxWidth: '600px',
-                  background: '#FFFFFF',
+                  background: '#940000',
                   boxShadow: '0px 14px 28.8px rgba(0, 0, 0, 0.24), 0px 0px 8px rgba(0, 0, 0, 0.2)',
                   borderRadius: '8px',
                   maxHeight: '200px',
