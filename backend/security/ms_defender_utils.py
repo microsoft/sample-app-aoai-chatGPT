8339484ecf127c863d3ebb36a500cf6b12b8a4b4
darnissan <darnis18@gmail.com>
2025-03-23 09:37:24 +0000

    fixing defaults to None

--- backend/security/ms_defender_utils.py
+++ backend/security/ms_defender_utils.py
@@ -5,18 +5,18 @@ import os
 
 @dataclass
 class UserSecurityContext:
-    application_name: str = field(default="default_app")    
-    end_user_id: str = field(default="00000000-0000-0000-0000-000000000000")
-    end_user_tenant_id: str = field(default="00000000-0000-0000-0000-000000000000")
-    source_ip: str = field(default="0.0.0.0")
-    
+    application_name: str = field(default=None)    
+    end_user_id: str = field(default=None)
+    end_user_tenant_id: str = field(default=None)
+    source_ip: str = field(default=None)
+
     def to_dict(self) -> Dict[str, Any]:
-        return asdict(self)
+        return {k: v for k, v in asdict(self).items() if v is not None}
             
 
-def get_msdefender_user_json(authenticated_user_details, request_headers, application_name, tenant_id) -> UserSecurityContext:
+def get_msdefender_user_json(authenticated_user_details, request_headers, application_name, end_user_tenant_id) -> UserSecurityContext:
     source_ip = request_headers.get('Remote-Addr', '')
     end_user_id = authenticated_user_details.get('user_principal_id')
     source_ip= source_ip.split(':')[0]
-    return UserSecurityContext(end_user_id=end_user_id, source_ip=source_ip, application_name=application_name, tenant_id=tenant_id)
+    return UserSecurityContext(end_user_id=end_user_id, source_ip=source_ip, application_name=application_name, end_user_tenant_id=end_user_tenant_id)
 
