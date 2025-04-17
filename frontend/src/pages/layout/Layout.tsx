63fd9cf746c28c5b80e50cc2fc20b07d9e4b1ba4
daveram123 <david.ramljak@pistor.ch>
2025-04-17 11:24:07 +0200

    Update Layout.tsx

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -4,7 +4,7 @@ import { Dialog, Stack, TextField } from '@fluentui/react'
 import { CopyRegular } from '@fluentui/react-icons'
 
 import { CosmosDBStatus } from '../../api'
-import Contoso from '../../assets/Contoso.svg'
+import PistorLogo from '../../assets/Pistor.png'
 import { HistoryButton, ShareButton } from '../../components/common/Button'
 import { AppStateContext } from '../../state/AppProvider'
 
