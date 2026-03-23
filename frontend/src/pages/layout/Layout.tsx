153112419f10fbac3066866d0f4b8e47fcaa485a
Sander H <Sander.halseth@orkland.kommune.no>
2026-03-23 13:00:53 +0100

    CSS change: center kommune karlsen banner/header, increase min height for text inside of chat window

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -75,7 +75,7 @@ const Layout = () => {
   return (
     <div className={styles.layout}>
       <header className={styles.header} role={'banner'}>
-        <Stack horizontal verticalAlign="center" horizontalAlign="space-between">
+        <Stack horizontal verticalAlign="center" horizontalAlign="center">
           <Stack horizontal verticalAlign="center">
             <img src={logo} className={styles.headerIcon} aria-hidden="true" alt="" />
             <Link to="/" className={styles.headerTitleContainer}>
